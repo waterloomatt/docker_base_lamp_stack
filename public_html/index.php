05fc15966e2eb7f44db8818fb9a23ab4ee0904bc
Matt Skelton <waterloomatt@gmail.com>
2019-11-19 10:49:21 -0500

    Add files via upload

--- public_html/index.php
+++ public_html/index.php
@@ -1,15 +1,64 @@
-<h1>Hello Cloudreach!</h1>
-<h4>Attempting MySQL connection from php...</h4>
+<pre>
 <?php
-$host = 'mysql';
-$user = 'root';
-$pass = 'rootpassword';
-$conn = new mysqli($host, $user, $pass);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Connected to MySQL successfully!";
+$people = [
+    'matt  ',
+    'jo    ',
+    'liz   ',
+    'bill  ',
+    'angela',
+];
+
+do {
+    $assignments = [];
+
+    foreach ($people as $person) {
+
+        $available = array_diff($people, $assignments, [$person]);
+
+        if (empty($available)) {
+            continue;
+        }
+
+        $assignments[$person] = $available[array_rand($available)];
+    }
+
+} while (count($people) !== count($assignments));
+
+print_r($assignments);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $host = 'mysql';
+    $db = 'development';
+    $user = 'root';
+    $pass = 'password';
+    $charset = 'utf8mb4';
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+    try {
+        $pdo = new PDO($dsn, $user, $pass, $options);
+    } catch (\PDOException $e) {
+        throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    }
+
+
+    $sql = "INSERT INTO users (name, surname, sex) VALUES (?,?,?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$name, $surname, $sex]);
+
 }
-
 ?>
+</pre>
+
+<form method="post">
+    <label>
+        <input type="text" name="email" placeholder="Email Address">
+    </label>
+
+    <input type="submit" value="Join the 2019 Gift Exchange"
+</form>
